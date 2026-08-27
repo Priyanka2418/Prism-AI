0b9ed1c57e7309f29ac88ad9c8ed51bd745088e4
Priyanka Gupta <priyanka142024@gmail.com>
2026-08-27 23:21:56 +0530

    feat(mentoring): support candidate and mentor request retrieval

--- src/main/java/com/aimock/interview/mentoring/request/repository/MentorRequestRepository.java
+++ src/main/java/com/aimock/interview/mentoring/request/repository/MentorRequestRepository.java
@@ -2,10 +2,14 @@ package com.aimock.interview.mentoring.request.repository;
 
 
 import com.aimock.interview.mentoring.request.entity.MentorRequest;
+import com.aimock.interview.mentoring.request.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.UUID;
 
 public interface MentorRequestRepository extends JpaRepository<MentorRequest, UUID> {
 
+    List<MentorRequest> findByMentorIdAndStatus(UUID mentorId, RequestStatus status);
+
+    List<MentorRequest> findByStudentId(UUID studentId);
 }
