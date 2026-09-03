793a6620ba1dfae917fbe4c8e3781951b907156c
Priyanka Gupta <priyanka142024@gmail.com>
2026-09-03 15:30:32 +0530

    feat(frontend): add routing for landing and authentication pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/landing/Navbar";
 import Hero from "./components/landing/Hero";
 import Marquee from "./components/landing/Marquee";
@@ -6,17 +8,32 @@ import Process from "./components/landing/Process";
 import FAQ from "./components/landing/FAQ";
 import Footer from "./components/landing/Footer";
 
-function App() {
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+function LandingPage() {
   return (
-    <>
+    <div className="scroll-smooth">
       <Navbar />
       <Hero />
       <Marquee />
       <Features />
       <Process />
-      <FAQ/>
-      <Footer/>
-    </>
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+         <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
